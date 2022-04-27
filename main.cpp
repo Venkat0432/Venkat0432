2972824072b60a9fcebbe79648852dcee5b22859
Venkat0432 <98548149+Venkat0432@users.noreply.github.com>
2022-04-26 20:09:42 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+- 👋 Hi, I’m @Venkat0432
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+Venkat0432/Venkat0432 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int count = 0;
+	int* array_of_int;
+	while (true)
+	{
+		count++;
+		array_of_int = new int[1000000];	
+		if (count % 100 == 0)
+		{
+			cout << count << endl;
+		}
+	
+	}
+	return 0;
+}
